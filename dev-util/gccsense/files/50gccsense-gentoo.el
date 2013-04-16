752447d833d206c0c05f4923666a65370e23611f
coldnew <coldnew.tw@gmail.com>
2013-04-16 19:36:19 +0800

    remove gcc-code-assist

--- dev-util/gccsense/files/50gccsense-gentoo.el
+++
@@ -1,6 +0,0 @@
-(add-to-list 'load-path "@SITELISP@")
-(autoload 'gccsense-complete "gccsense" "" t)
-(autoload 'gccsense-flymake-setup "gccsense" "" t)
-(autoload 'ac-complete-gccsense "gccsense" "" t)
-(autoload 'gccsense-diagnose "gccsense" "" t)
-
